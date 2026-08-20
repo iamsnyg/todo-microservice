c41d0cbf5cfbbc0993342c130de73311fc4ac9fa
Ubuntu <ubuntu@ip-172-31-27-149.ap-south-1.compute.internal>
2026-08-20 22:55:24 +0000

    fix error

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   /* config options here */
-=======
     output: "standalone",
->>>>>>> 616da95 (Add Todo microservices DevOps configuration)
 };
 
 export default nextConfig;
